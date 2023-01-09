ae5937d6d41d685cf5e19ff40c0de68383367929
Yu-Hsuan Wang <k43272001@gmail.com>
2023-01-09 15:44:18 +0800

    initial commit

---
+++ src/main/java/com/allyoushawn/mlservice/MlserviceApplication.java
@@ -0,0 +1,13 @@
+package com.allyoushawn.mlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlserviceApplication.class, args);
+	}
+
+}
