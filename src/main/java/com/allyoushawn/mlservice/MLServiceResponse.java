96596ad3a412303d642108fcbd3d2ece2d4dee5d
Yu-Hsuan Wang <k43272001@gmail.com>
2023-01-15 15:30:09 +0800

    add @JsonProperty to support objectMapper in downstream tasks

--- src/main/java/com/allyoushawn/mlservice/MLServiceResponse.java
+++ src/main/java/com/allyoushawn/mlservice/MLServiceResponse.java
@@ -1,12 +1,12 @@
 package com.allyoushawn.mlservice;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 @Data
 public class MLServiceResponse {
     private String status;
     private String requestId;
     private String content;
-
-    MLServiceResponse(String status, String requestId, String content){
+    MLServiceResponse(@JsonProperty("status")String status, @JsonProperty("requestId")String requestId, @JsonProperty("content")String content){
         this.status = status;
         this.requestId = requestId;
         this.content = content;
